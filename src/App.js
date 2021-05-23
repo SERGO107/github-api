694e0831823024d499a4787ad9e8e51f5c04045b
SERGO107 <sergserg10747@gmail.com>
2021-05-23 17:34:42 +0300

    dghj
    fghjkl

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Card, Image, Icon } from 'semantic-ui-react'
+import axios from 'axios';
+import { Form } from 'semantic-ui-react'
+import UserCard from './components/UserCard'
 import './App.css';
+import Pagination from './components/Pagination'
 
 function App() {
   const [name, setName] = useState('');
-  const [userName, setUsername] = useState('');
+  const [url, setUrl] = useState('');
+  const [userName, setUsername] = useState();
   const [followers, setFollowers] = useState('');
   const [following, setFollowing] = useState('');
   const [repos, setRepos] = useState('');
-  const [avatar, setAvatar] = useState('');
+  const [avatar, setAvatar] = useState('https://hsto.org/getpro/habr/post_images/ed8/a02/65d/ed8a0265d9c3767b08d8cb162a218939.jpg');
   const [userInput, setUserInput] = useState('');
   const [error, setError] = useState('');
+  const [mes, setMes] = useState('');
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/${name}`)
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-      })
+    function getFetchUrl() {
+      return `https://api.github.com/users/${name}`;
+    }
+    async function fetchData() {
+      const result = await axios(getFetchUrl());
+      setData(result.data);
+    }
+    fetchData();
   }, [])
-  const setData = ({ name, login, followers, following, public_repos, avatar_url }) => {
+
+  const setData = ({ name, login, followers, following, public_repos, avatar_url, html_url }) => {
     setName(name)
     setUsername(login)
     setFollowers(followers)
     setFollowing(following)
     setRepos(public_repos)
     setAvatar(avatar_url)
+    setUrl(html_url)
   }
   const handleSearch = (e) => {
     setUserInput(e.target.value)
+    let m = e.currentTarget.value
+    setMes(m)
   }
   const handleSubmit = () => {
     fetch(`https://api.github.com/users/${userInput}`)
@@ -36,58 +48,45 @@ function App() {
       .then(data => {
         if (data.message) {
           setError(data.message)
-        }else { 
-        setData(data)
-        setError(null)
+        } else {
+          setData(data)
+          setError(null)
         }
       })
+    setMes("")
   }
-
   return (
     <div >
-      <div className="navbar">Github Search</div>
-      <div className="search">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <Form.Input
-              placeholder='GIT user'
-              name='name'
-              onChange={handleSearch}
-            />
-            <Form.Button content='Search' />
-          </Form.Group>
-        </Form>
+      <div className="navbar">
+        Github Search
+      <div className="input">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Input
+                placeholder=''
+                name='name'
+                onChange={handleSearch}
+                value={mes}
+              />
+            </Form.Group>
+          </Form>
+        </div>
       </div>
-      {error ? (
-        <h1>{error}</h1>
-      ) : (
-        <div className="card">
-          <Card>
-            <Image src={avatar} wrapped ui={false} alt ="" />
-            <Card.Content>
-              <Card.Header>{name}</Card.Header>
-              <Card.Header>{userName}</Card.Header>
-            </Card.Content>
-            <Card.Content extra>
-              <a>
-                <Icon name='user' />
-                {followers} followers
-      </a>
-            </Card.Content>
-            <Card.Content extra>
-              <a>
-                <Icon name='user' />
-                {repos} repos
-      </a>
-            </Card.Content>
-            <Card.Content extra>
-              <a>
-                <Icon name='user' />
-                {following} following
-      </a>
-            </Card.Content>
-          </Card>
-        </div>)}
+
+      <UserCard
+        name={name}
+        userName={userName}
+        following={following}
+        followers={followers}
+        avatar={avatar}
+        repos={repos}
+        url={url}
+      />
+
+      <Pagination
+        login={userName}
+        repos={repos}
+      />
 
     </div>
   );
---
+++ src/components/Pagination.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import ReactPaginate from 'react-paginate';
+import './Pagination.css'
+
+function Pagination({ login }) {
+    const [offset, setOffset] = useState(0);
+    const [arr, setData] = useState([]);
+    const [perPage] = useState(10);
+    const [pageCount, setPageCount] = useState(0)
+
+    const getData = async () => {
+        const arr = []
+        await axios.all([
+            axios.get(`https://api.github.com/users/${login}/repos?page=2&per_page=100&page=1`),
+            axios.get(`https://api.github.com/users/${login}/repos?page=2&per_page=100&page=2`),
+            axios.get(`https://api.github.com/users/${login}/repos?page=2&per_page=100&page=3`),
+        ]).then(res => res.map(item => item.data))
+            .then(data => arr.push(...data.flat()))
+
+        const sliced = arr.slice(offset, offset + perPage)
+        const postData = sliced.map(pd => <div key={pd.id}>
+            <p><a href={pd.html_url}>{pd.name}</a></p>
+            <p>{pd.description}</p>
+        </div>)
+        setData(postData)
+        setPageCount(Math.ceil(arr.length / perPage))
+        console.log(arr)
+    }
+
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        setOffset(selectedPage *perPage)
+    };
+    useEffect(() => {
+        if (login == null) return null;
+        getData()
+    }, [offset, login])
+    return (
+        <div className="App">
+            {arr}
+            <ReactPaginate
+                previousLabel={"prev"}
+                nextLabel={"next"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                subContainerClassName={"pages pagination"}
+                activeClassName={"active"} />
+        </div>
+    );
+}
+export default Pagination;
